3a85e9b0f0e4312f8b0e2ea75de8fb0f06dfd9ca
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2012-08-24 19:10:14 -0500

    Initial commit of processing for geotiff.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-map-of-fire-and-ice
-===================
+# Map of Fire and Ice
 
-Playing around with image mapping with this map: http://www.sermountaingoat.co.uk/map/index.php
+Playing around with image mapping.  Map source is this awesome [map of the Song of Fire and Ice world by theMountainGoat](http://www.sermountaingoat.co.uk/map/index.php).  Techniques suggested by [tmcw](http://macwright.org/2012/08/13/images-as-maps.html).
+
+## Processing
+
+* Make GeoTIFF with: `python togeo.py speculative_map-1.0.jpg`
---
+++ togeo.py
@@ -0,0 +1,32 @@
+from subprocess import call, check_output
+import sys, re
+MERC = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs'
+
+infile = sys.argv[1]
+
+info_output = check_output(['gdalinfo', infile])
+
+size_is_re = re.compile('Size is (?P<width>\d+), (?P<height>\d+)')
+size_is = filter(lambda x: x, map(lambda x: size_is_re.match(x), info_output.split('\n')))
+
+if (len(size_is) != 1):
+    raise 'Could not parse gdalinfo output for image size'
+
+size = [float(size_is[0].group('width')), float(size_is[0].group('height'))]
+
+aspect_ratio = size[1] / size[0]
+
+# the full world dimension
+dim = 20037508.34 * 2
+
+if (aspect_ratio > 1):
+    h = dim
+    w = dim / aspect_ratio
+else:
+    h = dim * aspect_ratio
+    w = dim
+
+res = call(['gdal_translate', '-a_ullr', str(-w/2), str(-h/2), str(w/2), str(h/2), '-a_srs', MERC, '-co', 'COMPRESS=NONE', '-co', 'JPEG_QUALITY=100', infile, '%s.tif' % infile])
+
+if res != 0:
+    raise 'An error occurred upon calling gdal_translate'
